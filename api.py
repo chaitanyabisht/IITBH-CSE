45a994839e30abc27b1745059d45ce588b5e6ea4
Chaitanya Bisht <chaitanya.bisht10@gmail.com>
2022-03-02 10:45:02 +0530

    cached the results to increase speed

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import requests as r
+import streamlit as st
 
+@st.cache
 def course_list():
     lst = r.get('https://api.github.com/orgs/IIT-Bhilai-CSE/repos?type=all').json()
     return [item['name'] for item in lst]
