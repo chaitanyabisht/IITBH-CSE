f1ad1caee89f485818bcc20b726cbc101cf3899b
Chaitanya Bisht <chaitanya.bisht10@gmail.com>
2022-03-07 15:20:49 +0530

    Added GitHub API to list courses

--- cse_iitbh/cse_iitbh/views.py
+++ cse_iitbh/cse_iitbh/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import requests as r
+import requests as r
+
+def course_list():
+    lst = r.get('https://api.github.com/orgs/IIT-Bhilai-CSE/repos?type=all').json()
+    return [item['name'] for item in lst]
+
 
 def homepage(request):
     return render(request,'homepage.html')
@@ -9,4 +16,5 @@ def ycse(request):
     return render(request,'y_index.html')
 
 def courses(request):
-    return render(request, 'courses.html')
+    context = {"course_list":course_list()}
+    return render(request, 'courses.html', context)
