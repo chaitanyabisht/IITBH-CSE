16b61b5d5d9ca7728d44142085163ee199ebabfd
Chaitanya Bisht <chaitanya.bisht10@gmail.com>
2022-03-07 15:27:39 +0530

    Updated views.py functions

--- cse_iitbh/cse_iitbh/views.py
+++ cse_iitbh/cse_iitbh/views.py
@@ -8,7 +8,6 @@ def course_list():
     lst = r.get('https://api.github.com/orgs/IIT-Bhilai-CSE/repos?type=all').json()
     return [item['name'] for item in lst]
 
-
 def homepage(request):
     return render(request,'homepage.html')
 
