9d8e64fa09e161e6e684c454d9ad8244536488cc
Satvik Vemuganti <vsatvik2003@gmail.com>
2022-03-09 20:27:26 +0530

    bfs implementation template

---
+++ bfs.py
@@ -0,0 +1,22 @@
+graph = {}
+
+queue = []
+reached = []
+
+
+def bfs(queue, reached, start_vertex, end_vertex):
+    queue.append(start_vertex)
+    reached.append(start_vertex)
+
+    d = 0
+
+    while queue:
+        d += 1
+        x = queue.pop(0)
+
+        for i in graph[x]:
+            if i not in reached:
+                queue.append(i)
+                reached.append(i)
+            if i == end_vertex:
+                return d
