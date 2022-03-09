e41966cf48afa7d6335fdbcccc53e93312efd7ac
Chaitanya Bisht <chaitanya.bisht10@gmail.com>
2022-03-09 10:28:37 +0530

    resolved conflict for views

--- cse_iitbh/cse_iitbh/views.py
+++ cse_iitbh/cse_iitbh/views.py
@@ -1,25 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-<<<<<<< HEAD
-import requests as r
 import requests as r
 
 def course_list():
     lst = r.get('https://api.github.com/orgs/IIT-Bhilai-CSE/repos?type=all').json()
     return [item['name'] for item in lst]
-=======
->>>>>>> f35c2296129a6aa073b15555110d8cc9275c2d9d
+
 
 def homepage(request):
     return render(request,'homepage.html')
 
 def ycse(request):
     return render(request,'y_index.html')
-<<<<<<< HEAD
 
 def courses(request):
     context = {"course_list":course_list()}
     return render(request, 'courses.html', context)
-=======
->>>>>>> f35c2296129a6aa073b15555110d8cc9275c2d9d
+
