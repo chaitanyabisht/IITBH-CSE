e97db62e384eb7a883c46d0a522b0a89fd7ec608
Chaitanya Bisht <chaitanya.bisht10@gmail.com>
2022-03-09 19:44:06 +0530

    Updated wsgi.py

--- cse_iitbh/cse_iitbh/wsgi.py
+++ cse_iitbh/cse_iitbh/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cse_iitbh.settings')
 
 application = get_wsgi_application()
+
+if (__name__ == "__main__"):
+	main()
