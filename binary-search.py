e6dbff673ab3f8d13166067ae538ed394adfc363
Satvik Vemuganti <vsatvik2003@gmail.com>
2022-03-09 20:40:48 +0530

    undone the harm

--- binary-search.py
+++ binary-search.py
@@ -1,15 +1,15 @@
 def binary_search(arr, x):
-        down = 0
+        low = 0
         high = len(arr) - 1
         mid = 0
 
-        while down <= high:
+        while low <= high:
 
-            mid = (high + down) // 2
+            mid = (high + low) // 2
 
             # If x is greater, ignore left half
             if arr[mid] < x:
-                down = mid + 1
+                low = mid + 1
 
             # If x is smaller, ignore right half
             elif arr[mid] > x:
